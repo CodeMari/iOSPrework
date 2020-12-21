8a4cfa2bd02c8ea1f86a10f8cfbeaddd71e75bbc
CodeMari <amaril@uci.edu>
2020-12-21 11:03:05 -0800

    updated app - successful build

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -9,35 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var billAmountTextField: UITextField!
+    // @IBOutlet weak var tipPercentageLabel: UILabel!
     @IBOutlet weak var tipPercentageLabel: UILabel!
-    @IBOutlet weak var TipControl: UILabel!
-    
-    @IBAction func onTap(_ sender: Any) {
-    }
+    //   @IBOutlet weak var TipControl: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipAmountSegmentedControl: UISegmentedControl!
+   // @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     
     override func viewDidLoad() {
     
         super.viewDidLoad()
         // Do any additional setup after loading the view.
- 
- 
-func calculateTip(_ sender: Any) {
+    }
+    @IBAction func onTap(_ sender: Any) {
+    }
     
+ 
+    @IBAction func calculateTip(_ sender: Any) {
+    
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tipPercentages = [0.15,0.18,0.2]
+        
+        let tip = bill  * tipPercentages[tipAmountSegmentedControl.selectedSegmentIndex]
+     
+        let total = bill + tip
+        
+        tipPercentageLabel.text = String(format: "$%.f", tip)
+        
+        totalLabel.text = String(format: "$%.f", total)
+    
+    }
+    
+    }
 
 
-    let bill = Double(billAmountTextField.text!)?? 0
-let tipPercentages = [0.15, 0.18, 0.2]
-
-    let tip = bill*tipPercentages[tipAmountSegmentedControl.selectedSegmentIndex]
-
-let total = bill + tip
-
-tipPercentageLabel.text  = String(format: "$%.2f", tip)
-
-totalLabel.text = String(format: "$%.2f", total)
-
-
-}
-}
-}
